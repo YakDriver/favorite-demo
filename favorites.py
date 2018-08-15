cad63fb24929b83927835685d6c1a7a497359dde
Dirk Avery <dirk.avery@gmail.com>
2018-08-15 12:40:33 -0400

    Add new favorite functionality
    Fixes #1

--- favorites.py
+++ favorites.py
@@ -10,4 +10,8 @@ def get_fav(key):
 def display_fav(key):
     print("Favorite for",key,"is",get_fav(key))
 
+def add_fav(key, value):
+    favs[key] = value
+
 display_fav("color")
+add_fav("car", "Audi S4")
